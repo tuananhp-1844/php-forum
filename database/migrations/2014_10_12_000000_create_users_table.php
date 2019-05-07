faa4f49ad50e96815c8aea8bed6e008d2126b46f
tuananhp-1844 <phamtuananh760@gmail.com>
2019-05-07 16:07:22 +0700

    11788_Migration, Init Seeder

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,26 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
+            $table->increments('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->integer('point')->default(0);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('avatar')->default('');
+            $table->string('website')->nullable();
+            $table->string('facebook')->nullable();
+            $table->string('google')->nullable();
+            $table->string('linker')->nullable();
+            $table->string('twitter')->nullable();
+            $table->string('info')->nullable();
+            $table->string('country')->nullable();
+            $table->string('role_id');
+            $table->integer('status')->default(1);
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
---
+++ database/migrations/2019_05_07_083905_create_answers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAnswersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('content');
+            $table->integer('user_id');
+            $table->integer('parent_id')->default(0);
+            $table->integer('question_id');
+            $table->integer('is_best')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('answers');
+    }
+}
