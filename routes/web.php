099f073b117925ae85ba3d8cdce8be77d6162c35
tuananhp-1844 <phamtuananh760@gmail.com>
2019-05-08 15:31:31 +0700

    11790_home_page

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/', 'HomeController@index')->name('home');
+
+Route::resource('questions', 'Questions\QuestionController');
